3463e1808792a0fff53b487a9998f4af0617b10e
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-09-30 19:10:24 +0200

    Remove navbar black bg

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,9 +4,9 @@ import { FaCode } from 'react-icons/fa6';
 export default function Navbar() {
   const links = ['Home', 'Projects', 'Cv', 'Technologie', 'Contact'];
   return (
-    <nav className="flex items-center justify-around bg-black p-3 text-white">
+    <nav className="flex items-center justify-around  p-3 text-white">
       <h1 className="flex items-center gap-3 font-bold">
-        <FaCode size={30} className="text-cyan" /> Thibault Herve
+        <FaCode size={30} className="text-cyan" /> Portfolio
       </h1>
       <div>
         {links.map((link, index) => (
