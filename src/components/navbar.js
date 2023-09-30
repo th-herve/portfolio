94494233fbca3ae102f7674a9fe7cd02ed3c6288
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-09-30 19:39:02 +0200

    Made navbar links bigger

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,10 +5,10 @@ export default function Navbar() {
   const links = ['Home', 'Projects', 'Cv', 'Technologie', 'Contact'];
   return (
     <nav className="flex items-center justify-around  p-3 text-white">
-      <h1 className="flex items-center gap-3 font-bold">
+      <h1 className="flex items-center gap-3 text-2xl font-bold">
         <FaCode size={30} className="text-cyan" /> Portfolio
       </h1>
-      <div>
+      <div className="text-xl">
         {links.map((link, index) => (
           <Link text={link} key={index} />
         ))}
