8f67b54faea7ba49ea83b3cec554bf51573b0edd
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-09-30 19:11:43 +0200

    Add french description + image bg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Navbar from './components/navbar';
 import About from './components/about';
 
+const fullName = 'Thibault Hervé';
+const jobTitle = 'Développeur Junior';
+const introText =
+  "Je suis un développeur junior en quête d'un stage, animé par une passion dévorante pour le développement informatique. Je maîtrise déjà plusieurs technologies, prêt à mettre en pratique mes compétences pour enrichir votre équipe.";
+
+const aboutPage = <About fullName={fullName} jobTitle={jobTitle} description={introText} />;
+
 export default function App() {
   return (
-    <main className="h-screen">
+    <main
+      className="h-screen 
+       bg-[url('https://images.unsplash.com/photo-1448067686092-1f4f2070baae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')]"
+    >
       <Navbar />
-      <About />
+      {aboutPage}
     </main>
   );
 }
