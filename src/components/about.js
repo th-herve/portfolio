d9cb40aa02ea21c230ea2536e0de42317d8c581a
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-09-30 19:11:20 +0200

    Code about component

--- src/components/about.js
+++ src/components/about.js
@@ -1,3 +1,14 @@
-export default function About() {
-  return <div>test</div>;
+export default function About({ fullName, jobTitle, description }) {
+  const commonClassName = 'mb-5'; // the ones that can be inherited go into the wrapper div
+  return (
+    <div className="flex h-screen w-screen justify-center p-40">
+      <div className="text-6xl font-bold text-white">
+        <h1 className={commonClassName + ' text-cyan'}>Hello,</h1>
+        <h1 className={commonClassName + ' '}>I am {fullName}</h1>
+        <h2 className={commonClassName + ' text-5xl text-cyan'}>{jobTitle}</h2>
+        <p className={commonClassName + ' text-3xl'}>{description}</p>
+        <div></div>
+      </div>
+    </div>
+  );
 }
