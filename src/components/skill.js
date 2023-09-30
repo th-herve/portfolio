0c68cba20883459122f9568b7027ae22a3251718
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-09-30 22:43:22 +0200

    Add icon to the skill

--- src/components/skill.js
+++ src/components/skill.js
@@ -1,19 +1,34 @@
-import { FaReact, FaHtml5 } from 'react-icons/fa6';
+import { FaReact, FaHtml5, FaCss3Alt, FaJava, FaPhp, FaSquareJs, FaPython, FaGitAlt, FaLinux } from 'react-icons/fa6';
+import { SiGnubash, SiWebpack, SiJest, SiAdobephotoshop, SiAdobeindesign } from 'react-icons/si';
+import { PiFileSqlDuotone } from 'react-icons/pi';
 
-const iconComponent = [FaReact, FaHtml5];
+const iconComponent = [
+  FaReact,
+  FaHtml5,
+  FaCss3Alt,
+  FaJava,
+  FaPhp,
+  FaSquareJs,
+  FaPython,
+  FaGitAlt,
+  FaLinux,
+  SiGnubash,
+  SiWebpack,
+  SiJest,
+  SiAdobephotoshop,
+  SiAdobeindesign,
+  PiFileSqlDuotone,
+];
 
 export default function Skill() {
-  const iconSize = 50;
   return (
-    <div
-      className="
-                    bg-dark
-                    p-8 text-cyan"
-    >
-      <div className="flex rounded-md bg-black p-3">
+    <div className="flex w-screen bg-dark p-8 text-cyan">
+      <div className="grid grid-cols-5 gap-3 rounded-md bg-black p-3">
         {iconComponent.map((IconC, index) => (
-          <div className="rounded-md bg-gray p-2" key={'icon' + index}>
-            <IconC size={iconSize} />
+          <div className="flex" key={index}>
+            <div className="rounded-md bg-gray p-2" key={'icon' + index}>
+              <IconC size={100} />
+            </div>
           </div>
         ))}
       </div>
