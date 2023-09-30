23379c7956efa8a25886282b0b6728fd6a1f2b94
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-09-30 21:36:53 +0200

    Add sections in app and move bg img in about

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from './components/navbar';
+import Technologies from './components/technologies';
 import About from './components/about';
 import React, { useEffect } from 'react';
 
@@ -7,19 +8,19 @@ const jobTitle = 'Développeur Junior';
 const introText =
   "Je suis un développeur junior en quête d'un stage, animé par une passion pour le développement informatique. Je maîtrise déjà plusieurs technologies, et je suis prêt à mettre en pratique mes compétences pour enrichir votre équipe.";
 
-const aboutPage = <About fullName={fullName} jobTitle={jobTitle} description={introText} />;
-
 export default function App() {
   useEffect(() => {
     document.title = 'Portfolio';
   }, []);
   return (
-    <main
-      className="h-screen 
-       bg-[url('https://images.unsplash.com/photo-1448067686092-1f4f2070baae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')]"
-    >
+    <main>
       <Navbar />
-      {aboutPage}
+      <section className="about overflow-hidden">
+        <About fullName={fullName} jobTitle={jobTitle} description={introText} />
+      </section>
+      <section className="technologies ">
+        <Technologies />
+      </section>
     </main>
   );
 }
--- src/components/about.js
+++ src/components/about.js
@@ -1,8 +1,14 @@
 export default function About({ fullName, jobTitle, description }) {
   const commonClassName = 'mb-5'; // the ones that can be inherited go into the wrapper div
   return (
-    <div className="flex h-screen w-screen justify-center p-40">
-      <div className="text-6xl font-bold text-white">
+    <div
+      className="flex 
+                 w-screen 
+                 justify-center 
+                 bg-[url('https://images.unsplash.com/photo-1448067686092-1f4f2070baae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')] 
+                 p-40"
+    >
+      <div className=" text-6xl font-bold text-white">
         <h1 className={commonClassName + ' text-cyan'}>Hello,</h1>
         <h1 className={commonClassName + ' '}>I am {fullName}</h1>
         <h2 className={commonClassName + ' text-5xl text-cyan'}>{jobTitle}</h2>
