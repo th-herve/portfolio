341646a629bcbe74435730d989b5434e1583de33
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-10-01 15:14:45 +0200

    Made navbar a bit responsive

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,13 +3,15 @@ import { FaCode, FaXmark } from 'react-icons/fa6';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 import { useState } from 'react';
+const isMobile = window.innerWidth < 768;
 
 export default function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  console.log(isMobile);
 
   const linksClassName = function returnLinksCassName() {
-    let className = 'absolute top-12 w-screen p-8 text-center text-2xl text-white backdrop-blur-xl';
-    if (!isNavOpen) {
+    let className = 'absolute md:static md:flex top-12 w-screen p-8 text-center text-2xl text-white backdrop-blur-xl';
+    if (!isNavOpen && isMobile) {
       className += ' hidden';
     }
 
@@ -21,22 +23,22 @@ export default function Navbar() {
   return (
     <>
       <nav className="sticky top-0 flex items-center justify-around  bg-black p-3 text-2xl text-white">
-        <div className="mr-96 flex items-center gap-3 text-2xl font-bold">
+        <div className="mr-96 flex items-center gap-3 text-2xl font-bold md:mr-0">
           <button onClick={() => setIsNavOpen(!isNavOpen)} className="text-cyan">
             {isNavOpen ? <FaXmark size={30} /> : <GiHamburgerMenu size={30} />}
           </button>
           <FaCode size={30} className="text-cyan" />
         </div>
         <h1 className="hidden md:block">Portfolio</h1>
-        <div className="">Language</div>
 
         <ul className={linksClassName()}>
           {links.map((link, index) => (
-            <li key={link + index} className="mb-12">
+            <li key={link + index} className="mb-12 ">
               <Link text={link} />
             </li>
           ))}
         </ul>
+        <div className="">Language</div>
       </nav>
     </>
   );
