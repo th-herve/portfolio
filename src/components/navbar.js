8bdbc7e2516e78f3c0c6f4476011f072b26d48a7
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-10-01 15:52:44 +0200

    Made navbar fully responsive

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,36 +9,40 @@ export default function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
   console.log(isMobile);
 
-  const linksClassName = function returnLinksCassName() {
-    let className = 'absolute md:static md:flex top-12 w-screen p-8 text-center text-2xl text-white backdrop-blur-xl';
-    if (!isNavOpen && isMobile) {
-      className += ' hidden';
-    }
+  // const linksClassName = function returnLinksCassName() {
+  //   let className = 'absolute md:static md:flex top-12 w-screen p-8 text-center text-2xl text-white backdrop-blur-xl';
+  //   if (!isNavOpen && isMobile) {
+  //     className += ' hidden';
+  //   }
 
-    return className;
-  };
+  //   return className;
+  // };
+
   const links = ['Home', 'Projects', 'Cv', 'Technologie', 'Contact'];
 
   // note: there is a margin right apply on the right div in mobile mode, because if using anything other than justify-center on the navbar, the blur break on the right
   return (
     <>
-      <nav className="sticky top-0 flex items-center justify-around  bg-black p-3 text-2xl text-white">
+      <nav className="sticky top-0 flex items-center justify-around bg-black  px-8 py-6 text-2xl text-white">
         <div className="mr-96 flex items-center gap-3 text-2xl font-bold md:mr-0">
-          <button onClick={() => setIsNavOpen(!isNavOpen)} className="text-cyan">
+          <button onClick={() => setIsNavOpen(!isNavOpen)} className="text-cyan md:hidden">
             {isNavOpen ? <FaXmark size={30} /> : <GiHamburgerMenu size={30} />}
           </button>
           <FaCode size={30} className="text-cyan" />
         </div>
-        <h1 className="hidden md:block">Portfolio</h1>
+        <h1 className="ml-3  hidden lg:block">Portfolio</h1>
 
-        <ul className={linksClassName()}>
+        <ul
+          className={`${!isNavOpen ? 'hidden' : ''}
+            absolute top-12 w-screen text-center text-2xl text-white backdrop-blur-xl md:static md:flex md:justify-center lg:p-8`}
+        >
           {links.map((link, index) => (
-            <li key={link + index} className="mb-12 ">
+            <li key={link + index} className="mb-12 md:mb-0">
               <Link text={link} />
             </li>
           ))}
         </ul>
-        <div className="">Language</div>
+        <div className="">Fr</div>
       </nav>
     </>
   );
