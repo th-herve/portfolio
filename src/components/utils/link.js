f29230bdfc0002ba9c56b2839da499a1ed371d44
Thibault <thibault.herve1@gmail.com>
2023-10-02 13:10:51 +0200

    Use the href props in the link component

--- src/components/utils/link.js
+++ src/components/utils/link.js
@@ -1,6 +1,6 @@
 export default function Link({ text, key, href = '#' }) {
   return (
-    <a key={key} href={link} className="m-9 hover:text-cyan">
+    <a key={key} href={href} className="m-9 hover:text-cyan">
       {text}
     </a>
   );
