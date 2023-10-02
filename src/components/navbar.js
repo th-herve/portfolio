fa99beec38192170cfdc0d96525c160b5c1e9374
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-10-02 21:53:17 +0200

    Fix navbar link not centered

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -22,10 +22,10 @@ export default function Navbar() {
 
         <ul
           className={`${!isNavOpen ? 'hidden' : ''}
-            absolute right-0 top-20 w-screen text-center text-2xl text-white backdrop-blur-xl md:static md:flex md:justify-center`}
+            absolute right-0 top-20 w-screen text-center text-2xl text-white backdrop-blur-xl md:static md:flex  md:justify-center`}
         >
           {links.map((link, index) => (
-            <li key={link + index} className="mb-12 mt-12 md:mb-0">
+            <li key={link + index} className="mb-12 mt-12 md:my-0">
               <Link text={link} href={'#' + link.toLowerCase()} />
             </li>
           ))}
