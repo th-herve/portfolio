6677e8ddb1efd148ebddbc2f558f012c085a3fdd
Thibault <thibault.herve1@gmail.com>
2023-10-02 13:09:37 +0200

    Change the link prop to href

--- src/components/utils/link.js
+++ src/components/utils/link.js
@@ -1,4 +1,4 @@
-export default function Link({ text, key, link = '#' }) {
+export default function Link({ text, key, href = '#' }) {
   return (
     <a key={key} href={link} className="m-9 hover:text-cyan">
       {text}
