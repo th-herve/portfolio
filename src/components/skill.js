c882c0c9714a37a4cfe652a76b2e3361c71e5e9c
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-10-02 07:06:18 +0200

    Improve skills page layout

--- src/components/skill.js
+++ src/components/skill.js
@@ -3,14 +3,16 @@ import { skills } from '../data';
 export default function Skill() {
   return (
     <div className="mb-12 flex w-screen flex-col items-center bg-dark text-cyan">
-      <div className="mb-12 w-[85%] rounded-md bg-black p-3">
+      <div className="mb-12 w-[85%] rounded-md bg-black p-3 lg:w-[70%]">
         <div className="flex flex-col items-center">
           <div>
-            <h2 className="text-4xl font-bold">Skill</h2>
+            <div className="mb-10 border-b-2 border-solid border-cyan">
+              <h2 className="text-4xl font-bold">Skill</h2>
+            </div>
             {Object.keys(skills).map((category) => (
-              <div key={category}>
-                <h3>{category}</h3>
-                <div className="flex flex-wrap gap-3">
+              <div key={category} className="mb-10">
+                <h3 className="text-2xl">{category}</h3>
+                <div className="flex flex-wrap gap-3 lg:gap-10">
                   {skills[category].map((skill) => (
                     <div key={'icon' + skill}>
                       <div className="rounded-md bg-gray p-2">
