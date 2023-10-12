6120cecaab16d11662e6a085e920c3776379812e
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-10-12 19:29:48 +0200

    Add a margin between images

--- src/components/projects.js
+++ src/components/projects.js
@@ -9,19 +9,24 @@ export default function Projects() {
         <Title titleText="My projects" mb="0" />
       </div>
       <div className="lg:flex lg:h-screen lg:items-center lg:justify-center">
-        <div className="lg:flex lg:h-[800px] lg:justify-center lg:overflow-y-hidden">
+        <div id="ul-projects-container" className="lg:flex lg:h-[800px] lg:justify-center lg:overflow-y-hidden">
           <ul id="titlesList" className="hidden text-2xl text-white lg:flex lg:flex-col lg:justify-center">
             {projects.map((project, projectIndex) => (
               <li key={'li' + projectIndex}>{project.title}</li>
             ))}
           </ul>
 
-          <div className="flex snap-x snap-mandatory overflow-x-auto text-white " style={{ scrollbarWidth: 'none' }}>
+          <div
+            id="all-projects-container"
+            className="flex snap-x snap-mandatory overflow-x-auto text-white "
+            style={{ scrollbarWidth: 'none' }}
+          >
             <div className="flex px-[20%] lg:block lg:px-8">
               {projects.map((project, projectIndex) => (
                 <div
+                  id="single-project-container"
                   key={'div' + projectIndex}
-                  className="mx-10 my-5 flex min-w-[300px] snap-center flex-col items-center sm:min-w-[400px]"
+                  className="mx-10 my-5 flex min-w-[300px] snap-center flex-col items-center sm:min-w-[400px] lg:my-40" // gap between de image and on last
                 >
                   <h1 className="text-2xl lg:hidden">{project.title}</h1>
                   <div id="imageContainer" className="h-[200px] sm:h-[300px] lg:min-h-[500px]">
