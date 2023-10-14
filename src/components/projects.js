901973dda0ace9233f42eef2a451f9f78cd4df4f
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-10-14 18:56:33 +0200

    Fix image height pushing the description
    When reducing the size of a desktop view, it pushed the text away from the image.
    Using a given 'vh' value on the container fixed it!

--- src/components/projects.js
+++ src/components/projects.js
@@ -12,7 +12,7 @@ export default function Projects() {
       <div id="flex-wraper-lg" className="lg:ml-[10%] lg:flex lg:h-screen lg:items-center lg:justify-items-start">
         <div
           id="ul-projects-container"
-          className="lg:flex lg:h-[800px] lg:justify-center lg:gap-28 lg:overflow-y-hidden"
+          className="lg:flex lg:max-h-[60%] lg:justify-center lg:gap-28 lg:overflow-y-hidden"
         >
           <ul id="titlesList" className="hidden pt-0 text-2xl text-white lg:flex lg:flex-col">
             {projects.map((project, projectIndex) => (
@@ -33,7 +33,7 @@ export default function Projects() {
                   className="mx-10 my-5 flex min-w-[300px] snap-center flex-col items-center sm:min-w-[400px] lg:my-40 lg:snap-start lg:last:mb-80" // gap between desktop images and on last
                 >
                   <h1 className="text-2xl lg:hidden">{project.title}</h1>
-                  <div id="imageContainer" className="h-[200px] sm:h-[300px] lg:min-h-[500px]">
+                  <div id="imageContainer" className="h-[200px] sm:h-[300px] lg:h-[40vh]">
                     <img
                       className="max-h-[100%] max-w-xs rounded-md shadow-md shadow-dark sm:max-w-md lg:max-w-[100%]"
                       src={project.image}
