85fae2b95772ea5d50393067c15e201eeb10427f
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-10-17 18:54:08 +0200

    Add a show option in the skills list to hide them

--- src/components/skill.js
+++ src/components/skill.js
@@ -3,7 +3,7 @@ import Title from './utils/title';
 
 export default function Skill() {
   return (
-    <div className="mb-12 flex min-h-screen w-screen flex-col items-center bg-dark text-cyan">
+    <div className="flex min-h-screen w-screen flex-col items-center bg-dark text-cyan">
       <div className="mb-12 w-[85%] rounded-md bg-black p-3 lg:w-[70%]">
         <div className="flex flex-col items-center">
           <div>
@@ -12,13 +12,17 @@ export default function Skill() {
               <div key={category} className="mb-10">
                 <h3 className="text-2xl">{category}</h3>
                 <div className="flex flex-wrap gap-3 lg:gap-10">
-                  {skills[category].map((skill, index) => (
-                    <div key={'icon' + index}>
-                      <div className="rounded-md bg-gray p-2">
-                        <skill.icon size="4rem" />
+                  {skills[category].map((skill, index) =>
+                    skill.show ? (
+                      <div key={'icon' + index}>
+                        <div className="rounded-md bg-gray p-2">
+                          <skill.icon size="4rem" />
+                        </div>
                       </div>
-                    </div>
-                  ))}
+                    ) : (
+                      ''
+                    ),
+                  )}
                 </div>
               </div>
             ))}
--- src/data/data_skills.js
+++ src/data/data_skills.js
@@ -19,104 +19,125 @@ export const skills = (() => {
     other: 'Others',
   };
 
+  // The icon entry is a react component
   const skillsList = [
     {
       name: 'HTML5',
       category: categories.language,
       icon: FaHtml5,
+      show: true,
     },
     {
       name: 'C',
       category: categories.language,
       icon: SiC,
+      show: true,
     },
     {
       name: 'Bootstrap',
       category: categories.framework,
       icon: SiBootstrap,
+      show: true,
     },
     {
       name: 'Tailwind',
       category: categories.framework,
       icon: SiTailwindcss,
+      show: true,
     },
     {
       name: 'Flask',
       category: categories.framework,
       icon: SiFlask,
+      show: true,
     },
     {
       name: 'React',
       category: categories.framework,
       icon: FaReact,
+      show: true,
     },
     {
       name: 'CSS3',
       category: categories.language,
       icon: FaCss3Alt,
+      show: true,
     },
     {
       name: 'Java',
       category: categories.language,
       icon: FaJava,
+      show: true,
     },
     {
       name: 'PHP',
       category: categories.language,
       icon: FaPhp,
+      show: true,
     },
     {
       name: 'Javascript',
       category: categories.language,
       icon: FaSquareJs,
+      show: true,
     },
     {
       name: 'Python',
       category: categories.language,
       icon: FaPython,
+      show: true,
     },
     {
       name: 'Git',
       category: categories.other,
       icon: FaGitAlt,
+      show: true,
     },
     {
       name: 'Linux',
       category: categories.other,
       icon: FaLinux,
+      show: true,
     },
     {
       name: 'Bash',
       category: categories.other,
       icon: SiGnubash,
+      show: true,
     },
     {
       name: 'Webpack',
       category: categories.other,
       icon: SiWebpack,
+      show: false,
     },
     {
       name: 'Jest',
       category: categories.other,
       icon: SiJest,
+      show: false,
     },
     {
       name: 'Adobe Photoshop',
       category: categories.other,
       icon: SiAdobephotoshop,
+      show: true,
     },
     {
       name: 'Adobe InDesign',
       category: categories.other,
       icon: SiAdobeindesign,
+      show: true,
     },
     {
       name: 'SQL',
       category: categories.language,
       icon: PiFileSqlDuotone,
+      show: true,
     },
   ];
 
+  // groupe the skills by category before exporting to make it easier to work with
   const groupedSkills = skillsList.reduce((acc, skill) => {
     acc[skill.category] = acc[skill.category] || [];
     acc[skill.category].push(skill);
