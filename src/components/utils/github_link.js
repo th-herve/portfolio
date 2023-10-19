9b5899f2ccb0d01da4e8047fac563f05672284cb
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-10-19 18:11:45 +0200

    Github links component now open in a new tab

--- src/components/utils/github_link.js
+++ src/components/utils/github_link.js
@@ -2,7 +2,7 @@ import { FaSquareGithub } from 'react-icons/fa6';
 
 export default function GithubLink({ href = '' }) {
   return (
-    <a href={href}>
+    <a href={href} target="_blank" rel="noreferrer">
       <FaSquareGithub size="50px" className="text-white" />
     </a>
   );
