ac6055750b08e9a48e724eb4cfc95032642df925
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-10-19 19:44:12 +0200

    Fix the desktop links not working on different viewport height

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,21 +5,34 @@ import LiBig from './utils/liBig';
 
 import { useRef, useEffect, useState } from 'react';
 
+// Value of the first scrollable item that offset the calcul of the items height.
+// Can be obtain with 'document.getElementById('single-project-container-0').getBoundingClientRect().top; '
+// But this value changes after we start scrolling. So for now it's easier to hard code it.
+const SCROLL_OFFSET = 160;
+
 export default function Projects() {
-  const SCROLLOFFSET = 608;
   const [imageIndex, setImageIndex] = useState(0);
   const scrollableDivRef = useRef();
 
+  function getScrollItemHeight() {
+    const scrollItemHeight = document.getElementById('single-project-container-0').offsetHeight;
+    return scrollItemHeight + SCROLL_OFFSET;
+  }
+
+  // callback for the project links. Scroll to the corresponding image for the given index.
   const setIndexToImageClicked = (imageIndex) => {
-    scrollableDivRef.current.scrollTop = imageIndex * SCROLLOFFSET;
+    const itemHeight = getScrollItemHeight();
+    scrollableDivRef.current.scrollTop = imageIndex * itemHeight;
   };
 
   const updateImageIndexOnScroll = () => {
+    const itemHeight = getScrollItemHeight();
+
     // Calculate the scroll position of the scrolable div
     const scrollPosition = scrollableDivRef.current.scrollTop;
 
     // Calculate the image index based on scroll position
-    const newImageIndex = Math.floor(scrollPosition / SCROLLOFFSET);
+    const newImageIndex = Math.floor(scrollPosition / itemHeight);
 
     setImageIndex(newImageIndex);
   };
@@ -69,7 +82,7 @@ export default function Projects() {
                   className="mx-10 my-5 flex min-w-[300px] snap-center flex-col items-center sm:min-w-[400px] lg:my-40 lg:snap-start lg:last:mb-80" // gap between desktop images and on last
                 >
                   <h1 className="text-2xl lg:hidden">{project.title}</h1>
-                  <div id="imageContainer" className="h-[200px] sm:h-[300px] lg:h-[40vh]">
+                  <div id="imageContainer" className="flex h-[200px] items-center sm:h-[300px] lg:h-[40vh]">
                     <img
                       className="max-h-[100%] max-w-xs rounded-md shadow-md shadow-dark sm:max-w-md lg:max-w-[100%]"
                       src={project.image}
@@ -78,7 +91,7 @@ export default function Projects() {
                   </div>
                   <div className="mt-2 flex items-center">
                     <p className="text-start">{project.description}</p>
-                    <GithubLink href="#" />
+                    <GithubLink href={project.githubLink} />
                   </div>
                 </div>
               ))}
