edd94fae32dc8eb092fde3db6938acd8bc7e2cdf
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-10-29 15:26:51 +0100

    Fix skill hovering box text overflowin

--- src/components/utils/skillBox.js
+++ src/components/utils/skillBox.js
@@ -15,9 +15,13 @@ export default function SkillBox({ IconComponent, skillName, level, size = '4rem
         <IconComponent size={size} />
 
         {isShown && (
-          <div id="legend" className="absolute bottom-24  left-0 right-0 m-auto rounded-md bg-gray p-3 text-center">
+          <div
+            id="legend"
+            className="absolute bottom-24  left-0 right-0 m-auto rounded-md border-2 border-cyan bg-gray p-1 py-3 text-center"
+            style={{ overflow: 'visible' }}
+          >
             <p>{skillName}</p>
-            <div className="h-2.5 w-full rounded-full border-dark bg-dark shadow-md shadow-cyan">
+            <div className="h-2.5 w-full rounded-full border-dark bg-dark shadow-sm shadow-cyan">
               <div className="h-2.5 rounded-full bg-cyan " style={levelMeterWidth}></div>
             </div>
           </div>
