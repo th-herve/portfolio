7b6b4bbc7d54359d791b57f45ad6b355e745f2af
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-10-29 15:45:14 +0100

    Made the navabar take the whole screen on mobile when open

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -10,28 +10,26 @@ export default function Navbar() {
   const links = ['Home', 'Projects', 'Skills', 'Contact'];
 
   return (
-    <>
-      <nav className="sticky top-0 flex items-center justify-between bg-black  px-8 py-3 text-2xl text-white">
-        <div className="flex items-center gap-3 text-2xl font-bold ">
-          <button onClick={() => setIsNavOpen(!isNavOpen)} className="text-cyan md:hidden">
-            {isNavOpen ? <FaXmark size="2rem" /> : <GiHamburgerMenu size="2rem" />}
-          </button>
-          <FaCode size="2rem" className="text-cyan" />
-        </div>
-        <h1 className="ml-3  hidden lg:block">Portfolio</h1>
+    <nav className="sticky top-0 flex items-center justify-between bg-black  px-8 py-3 text-2xl text-white">
+      <div className="flex items-center gap-3 text-2xl font-bold ">
+        <button onClick={() => setIsNavOpen(!isNavOpen)} className="text-cyan md:hidden">
+          {isNavOpen ? <FaXmark size="2rem" /> : <GiHamburgerMenu size="2rem" />}
+        </button>
+        <FaCode size="2rem" className="text-cyan" />
+      </div>
+      <h1 className="ml-3  hidden lg:block">Portfolio</h1>
 
-        <ul
-          className={`${!isNavOpen ? 'hidden' : ''}
-            absolute right-0 top-14 w-screen text-center text-2xl text-white backdrop-blur-xl md:static md:flex md:justify-center`}
-        >
-          {links.map((link, index) => (
-            <li key={link + index} className="mb-12 mt-12 md:my-0">
-              <Link text={link} href={'#' + link.toLowerCase()} />
-            </li>
-          ))}
-        </ul>
-        <div className="">Fr</div>
-      </nav>
-    </>
+      <ul
+        className={`${!isNavOpen ? 'hidden' : ''}
+            absolute right-0 top-14 h-screen w-screen text-center text-2xl text-white backdrop-blur-xl md:static md:flex md:h-auto md:justify-center`}
+      >
+        {links.map((link, index) => (
+          <li key={link + index} className="mb-12 mt-12 md:my-0">
+            <Link text={link} href={'#' + link.toLowerCase()} />
+          </li>
+        ))}
+      </ul>
+      <div className="">Fr</div>
+    </nav>
   );
 }
