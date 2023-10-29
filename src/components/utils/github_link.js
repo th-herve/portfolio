b81f5ccf836698379b3ded2962ce91cf53885d13
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-10-29 18:06:59 +0100

    Change github links to round

--- src/components/utils/github_link.js
+++ src/components/utils/github_link.js
@@ -1,9 +1,9 @@
-import { FaSquareGithub } from 'react-icons/fa6';
+import { FaGithub } from 'react-icons/fa6';
 
 export default function GithubLink({ href = '' }) {
   return (
     <a href={href} target="_blank" rel="noreferrer">
-      <FaSquareGithub size="50px" className="text-white" />
+      <FaGithub size="50px" className="text-white" />
     </a>
   );
 }
