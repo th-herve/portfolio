84c2675ed8183d115135dd09a0b415db18521502
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-11-05 18:55:48 +0100

    Add right link to cycling project

--- src/data/data_projects.js
+++ src/data/data_projects.js
@@ -14,7 +14,7 @@ export const projects = [
     image: cyclingImg,
     description:
       'My first programming project, a straightforward web application that presents professional cycling races scheduled in a calendar format. Built using Flask for the backend and sport radar api to retrieve the data.',
-    link: 'cycling.th-herve.fr',
+    link: 'https://cycling.th-herve.fr',
     githubLink: 'https://github.com/th-herve/Cycling-calendar',
   },
   // {
