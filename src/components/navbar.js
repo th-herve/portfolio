ee19f5470ef4304b4a66f2c48845e8448ff95976
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-11-12 18:52:15 +0100

    Added comment to the navbar component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,3 +1,4 @@
+// Reactive navbar with hamburger menu on mobile and normal navbar on desktop
 import Link from './utils/link.js';
 import { FaCode, FaXmark } from 'react-icons/fa6';
 import { GiHamburgerMenu } from 'react-icons/gi';
@@ -8,18 +9,22 @@ import GithubLink from './utils/github_link.js';
 export default function Navbar() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
+  // list of navigations links
   const links = ['Home', 'Projects', 'Skills', 'Contact'];
 
   return (
     <nav className="sticky top-0 flex items-center justify-between bg-black  px-8 py-3 text-2xl text-white">
       <div className="flex items-center gap-3 text-2xl font-bold ">
+        {/* button that toggle the hamburger menu, display a X if open or the hamberger icon if closed, hidden on desktop */}
         <button onClick={() => setIsNavOpen(!isNavOpen)} className="text-cyan md:hidden">
           {isNavOpen ? <FaXmark size="2rem" /> : <GiHamburgerMenu size="2rem" />}
         </button>
+        {/* </> icon */}
         <FaCode size="2rem" className="text-cyan" />
       </div>
       <h1 className="ml-3  hidden lg:block">Portfolio</h1>
 
+      {/* hamburger menu implementation */}
       <ul
         className={`${!isNavOpen ? 'hidden' : ''}
             absolute right-0 top-14 h-screen w-screen text-center text-2xl text-white backdrop-blur-xl md:static md:flex md:h-auto md:justify-center`}
@@ -30,6 +35,7 @@ export default function Navbar() {
           </li>
         ))}
       </ul>
+
       <GithubLink href="https://github.com/th-herve/" size="40px" />
     </nav>
   );
