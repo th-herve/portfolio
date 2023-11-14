67a734daaff1621ca0415942e9a920802ebacc73
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-11-14 21:35:41 +0100

    Add forwardRef to LiBig component

--- src/components/utils/liBig.js
+++ src/components/utils/liBig.js
@@ -1,13 +1,28 @@
-export default function LiBig({ text, className, selected = false, href = '#' }) {
-  // note: the key must be defined when calling the component
+import { forwardRef } from 'react';
 
+// forwardRef allows to create ref in the parent elements
+export default forwardRef(function LiBig({ text, className, selected = false, href = '#' }, ref) {
   let textColor = selected ? 'text-cyan' : 'text-white';
   className = textColor + ' ' + 'text-4xl py-4 pl-20 text-right border-white border-t-2' + ' ' + className;
   let capitalizeText = text[0].toUpperCase() + text.substring(1, text.length).toLowerCase();
 
   return (
-    <li href={href} className={className}>
+    <li ref={ref} href={href} className={className}>
       {capitalizeText}
     </li>
   );
-}
+});
+
+// export default function LiBig({ text, className, selected = false, href = '#' }) {
+//   // note: the key must be defined when calling the component
+
+//   let textColor = selected ? 'text-cyan' : 'text-white';
+//   className = textColor + ' ' + 'text-4xl py-4 pl-20 text-right border-white border-t-2' + ' ' + className;
+//   let capitalizeText = text[0].toUpperCase() + text.substring(1, text.length).toLowerCase();
+
+//   return (
+//     <li href={href} className={className}>
+//       {capitalizeText}
+//     </li>
+//   );
+// }
