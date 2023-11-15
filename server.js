12a0e56223ab9ef6dce364e32b0f426ecdb83368
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-11-15 16:29:00 +0100

    Add post route for the contact form

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3001; // You can change the port if needed
@@ -7,9 +9,45 @@ const port = process.env.PORT || 3001; // You can change the port if needed
 // Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'build')));
 
+// Parse incoming JSON requests
+app.use(bodyParser.json());
+
 // Handle any other routes by serving the index.html file
-app.get('*', (req, res) => {
+app.get('*', (_, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.post('/api/contact', (req, res) => {
+  console.log('Request Body:', req.body);
+
+  const dataFilePath = path.join(__dirname, 'contact.json');
+
+  const { firstName, lastName, email, message } = req.body;
+
+  const formData = {
+    firstName,
+    lastName,
+    email,
+    message,
+    timestamp: new Date(),
+  };
+
+  // Read existing data from the file
+  let existingData = [];
+  try {
+    existingData = JSON.parse(fs.readFileSync(dataFilePath));
+  } catch (error) {
+    console.error('Error reading data file:', error);
+  }
+
+  // Add new form data to the existing data
+  existingData.push(formData);
+
+  // Write the updated data back to the file
+  fs.writeFileSync(dataFilePath, JSON.stringify(existingData, null, 2));
+
+  // Send a success response
+  res.json({ success: true });
 });
 
 // Start the server
--- src/components/utils/textInput.js
+++ src/components/utils/textInput.js
@@ -1,4 +1,13 @@
-export default function Input({ id, placeholder, name, type = 'text', hasLabel = false, labelText = '' }) {
+export default function Input({
+  id,
+  placeholder,
+  name,
+  value,
+  onChange,
+  type = 'text',
+  hasLabel = false,
+  labelText = '',
+}) {
   return (
     <>
       {hasLabel ? (
@@ -15,6 +24,8 @@ export default function Input({ id, placeholder, name, type = 'text', hasLabel =
         id={id}
         placeholder={placeholder}
         name={name}
+        value={value}
+        onChange={onChange}
       ></input>
     </>
   );
