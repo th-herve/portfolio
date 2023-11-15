67b7c74e947692ae3b7974556fd6cc71c16d2c4c
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-11-15 11:38:03 +0100

    Clean up about page

--- src/components/about.js
+++ src/components/about.js
@@ -3,15 +3,14 @@ import { useTranslation } from 'react-i18next';
 export default function About() {
   const fullName = 'Thibault Hervé';
 
-  const commonClassName = 'mb-5'; // the ones that can be inherited go into the wrapper div
   const { t } = useTranslation();
   return (
     <div className="flex h-screen w-[80%] justify-center bg-dark py-10 md:p-20">
       <div className="text-6xl font-bold text-white ">
-        <h1 className={commonClassName + ' text-cyan'}>{t('about.hello')}</h1>
-        <h1 className={commonClassName + ' text-3xl md:text-5xl'}>{t('about.IAm') + fullName}</h1>
-        <h2 className={commonClassName + ' text-3xl text-cyan md:text-5xl'}>{t('about.jobTitle')}</h2>
-        <p className={commonClassName + ' text-xl md:text-3xl'}>{t('about.description')}</p>
+        <h1 className="mb-5 text-cyan">{t('about.hello')}</h1>
+        <h1 className="mb-5 text-3xl md:text-5xl">{t('about.IAm') + fullName}</h1>
+        <h2 className="mb-5 text-3xl text-cyan md:text-5xl">{t('about.jobTitle')}</h2>
+        <p className="mb-5 text-xl md:text-3xl">{t('about.description')}</p>
       </div>
     </div>
   );
