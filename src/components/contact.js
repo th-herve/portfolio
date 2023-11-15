be53f2c143d680c7a51bcc71ea476aa32317b9c3
Thibault Herve <126357296+th-herve@users.noreply.github.com>
2023-11-15 11:37:48 +0100

    Add translation for the contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,26 +1,47 @@
+import { useTranslation } from 'react-i18next';
+
 import Input from './utils/textInput';
 import Title from './utils/title';
 
 // TODO Have the form and textarea be their own components
 
 export default function Contact() {
+  const { t } = useTranslation();
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-r from-black to-gray">
       <form action="" method="" className="p-5  ">
-        <Title titleText="Contact" />
+        <Title titleText={t('contact.title')} />
         <div className="mb-10">
           <div className="flex gap-3">
             <div>
-              <Input id="firstName" placeholder="First name" name="firstName" hasLabel={true} labelText="First name" />
+              <Input
+                id="firstName"
+                placeholder={t('contact.firstNamePlaceHolder')}
+                name="firstName"
+                hasLabel={true}
+                labelText={t('contact.firstName')}
+              />
             </div>
             <div>
-              <Input id="lastName" placeholder="Last name" name="lastName" hasLabel={true} labelText="Last name" />
+              <Input
+                id="lastName"
+                placeholder={t('contact.lastNamePlaceHolder')}
+                name="lastName"
+                hasLabel={true}
+                labelText={t('contact.lastName')}
+              />
             </div>
           </div>
-          <Input id="email" placeholder="yourmail@mail.com" name="email" hasLabel={true} labelText="Email" />
+          <Input
+            id="email"
+            placeholder={t('contact.emailPlaceHolder')}
+            name="email"
+            hasLabel={true}
+            labelText={t('contact.email')}
+          />
 
           <label className="font-bold text-cyan" htmlFor="message">
-            Your message
+            {t('contact.message')}
           </label>
           <textarea
             id="message"
@@ -34,7 +55,7 @@ export default function Contact() {
               className="rounded-md border-2 border-cyan bg-black px-9 py-3 font-bold text-cyan transition ease-in-out hover:shadow-md hover:shadow-cyan hover:transition-all "
               type="submit"
             >
-              Send
+              {t('contact.send')}
             </button>
           </div>
         </div>
